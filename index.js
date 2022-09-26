7db5049de192b513a5eb0757637bd52ba510f1b3
chetan shinde <chetanshinde51423@gmail.com>
2022-09-26 23:59:35 +0530

    snake function

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 //game const and varibles
 
-let direction = {x: 0, y: 0};
+let inputDir = {x: 0, y: 0};
 const  foodSound = new Audio("music/food.mp3");
 const  gameOver = new Audio("music/gameover.mp3");
 const  moveSound = new Audio("music/move.mp3");
 const  musicSound = new Audio("music/music.mp3");
 let speed=2;
+let score =0;
 let lastPaintTime=0;
 let snakeArr = [
     {x:13, y:15}
-]
+];
 food= {x:6,y:7};
 
 
@@ -24,11 +25,30 @@ function main(ctime)
     gameEngine();
     
 }
-
+function isCollide(sarr)
+{
+    return false;
+}
 function gameEngine()
 {
     //part:1 upadting the snake array and food
-     
+     if(isCollide(snakeArr))
+     {
+        gameOverSound.play();
+        musicSound.pause();
+        inputDir = {x: 0, y: 0};
+        alert("Game Over! press any key to start again!");
+        snakeArr = [{x:13, y:15}];
+        musicSound.play();
+        score =0;
+
+     }
+
+     //if you ave eaten the food, increment the score and regenerate the food
+     if(snakeArr[0].y===food.y && snakeArr[0].x===food.x)
+     {
+        snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y})
+     }
 
 
 
@@ -68,3 +88,27 @@ function gameEngine()
 }
 //main logic starts here
 window.requestAnimationFrame(main);
+window.addEventListener('keydown', e=>{
+    inputDir = {x:0,y:1}    //strat the game
+    moveSound.play();
+    switch(e.key)
+    {
+        case "ArrowUp":
+            inputDir.x=0;
+            inputDir.y=-1;
+            break;
+        case "ArrowDown":
+            inputDir.x=0;
+            inputDir.y=1;
+            break;
+        case "ArrowLeft":
+            inputDir.x=-1;
+            inputDir.y=0;
+            break;
+        case "ArrowRight":
+            inputDir.x=1;
+            inputDir.y=0;
+            break;
+        
+    }
+})
